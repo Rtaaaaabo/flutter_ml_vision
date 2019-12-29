ba8c5c5956ca4cc674cc1cc28235016a3b81ac6f
Taku Nakagawa <takunakagawa@TakunoMac-mini.local>
2019-12-29 19:03:36 +0900

    Home Stateの削除

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 // import 'dart:html';
 
-import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 import 'package:flutter_camera_ml_vision/flutter_camera_ml_vision.dart';
 
 void main() => runApp(MyApp());
@@ -20,56 +19,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  List<String> data = [];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(widget.title)),
-      body: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          RaisedButton(
-            child: Text('スキャン'),
-            onPressed: () async {
-              final barcode = await Navigator.of(context).push<Barcode>(
-                MaterialPageRoute(
-                  builder: (c) {
-                    return ScanPage();
-                  },
-                ),
-              );
-              if (barcode == null) {
-                return;
-              }
-              setState(() {
-                print(barcode);
-                data.add(barcode.displayValue);
-              });
-            },
-          ),
-          Expanded(
-            child: ListView(
-              children: data.map((d) => Text(d)).toList(),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
 class ScanPage extends StatefulWidget {
   final String title;
   ScanPage({Key key, this.title}) : super(key: key);
@@ -79,8 +28,7 @@ class ScanPage extends StatefulWidget {
 
 class _ScanPageState extends State<ScanPage> {
   bool resultSent = false;
-  BarcodeDetector barcodeDetector = FirebaseVision.instance.barcodeDetector();
-  FaceDetector faceDetector = FirebaseVision.instance.faceDetector();
+  final FaceDetector faceDetector = FirebaseVision.instance.faceDetector();
 
   @override
   Widget build(BuildContext context) {
@@ -90,7 +38,7 @@ class _ScanPageState extends State<ScanPage> {
         width: MediaQuery.of(context).size.width,
         child: CameraMlVision<List<Face>>(
           detector: faceDetector.processImage,
-          onResult: (infoFaces) {
+          onResult: (List<Face> infoFaces) {
             if (!mounted ||
                 resultSent ||
                 infoFaces == null ||
