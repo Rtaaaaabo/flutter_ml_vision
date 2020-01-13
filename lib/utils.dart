13068054b5867d1ecf097cf39382717ce212b051
Taku Nakagawa <takunakagawa@TakunoMac-mini.local>
2020-01-13 22:06:44 +0900

    Util.dart内をコメントアウト

--- lib/utils.dart
+++ lib/utils.dart
@@ -2,35 +2,35 @@ import 'package:camera/camera.dart';
 import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 import 'package:meta/meta.dart';
 
-bool isDetecting = false;
+// bool isDetecting = false;
 
-// Future<CameraDescription>
-Future<CameraDescription> getFrontCamera() async {
-  return await availableCameras().then((cameras) => cameras.firstWhere(
-      (camera) => camera.lensDirection == CameraLensDirection.front));
-}
+// // Future<CameraDescription>
+// Future<CameraDescription> getFrontCamera() async {
+//   return await availableCameras().then((cameras) => cameras.firstWhere(
+//       (camera) => camera.lensDirection == CameraLensDirection.front));
+// }
 
-void cameraBytesToDetector({
-  @required CameraController camera,
-  @required FaceDetector detector,
-  @required void updateFace(Face face),
-}) {
-  camera.startImageStream((image) {
-    if (isDetecting) return;
+// void cameraBytesToDetector({
+//   @required CameraController camera,
+//   @required FaceDetector detector,
+//   @required void updateFace(Face face),
+// }) {
+//   camera.startImageStream((image) {
+//     if (isDetecting) return;
 
-    isDetecting = true;
+//     isDetecting = true;
 
-    detector.processImage(FirebaseVisionImage.fromBytes(
-        image.planes[0].bytes,
-        FirebaseVisionImageMetadata(
-            rawFormat: image.format.raw,
-            size: camera.value.previewSize,
-            rotation: ImageRotation.rotation270,
-            planeData: image.planes
-                .map((currentPlane) => FirebaseVisionImagePlaneMetadata(
-                    bytesPerRow: currentPlane.bytesPerRow,
-                    height: currentPlane.height,
-                    width: currentPlane.width))
-                .toList())));
-  });
-}
+//     detector.processImage(FirebaseVisionImage.fromBytes(
+//         image.planes[0].bytes,
+//         FirebaseVisionImageMetadata(
+//             rawFormat: image.format.raw,
+//             size: camera.value.previewSize,
+//             rotation: ImageRotation.rotation270,
+//             planeData: image.planes
+//                 .map((currentPlane) => FirebaseVisionImagePlaneMetadata(
+//                     bytesPerRow: currentPlane.bytesPerRow,
+//                     height: currentPlane.height,
+//                     width: currentPlane.width))
+//                 .toList())));
+//   });
+// }
