169db41b6ca9bfd3b3f0b48329b6fe44914316f2
Taku Nakagawa <takunakagawa@TakunoMac-mini.local>
2020-01-16 00:30:08 +0900

    face_detection_cameraの作成

--- lib/smile_painter.dart
+++ lib/smile_painter.dart
@@ -65,4 +65,59 @@ class SmilePainterLiveCamera extends CustomPainter {
   final List<Face> faces;
 
   SmilePainterLiveCamera(this.imageSize, this.faces);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()..color = Colors.yellow;
+    for (var i = 0; i < faces.length; i++) {
+      final rect = _scaleRect(
+        rect: faces[i].boundingBox,
+        imageSize: imageSize,
+        widgetSize: size,
+      );
+
+      final radius = Math.min(rect.width, rect.height) / 2;
+      final Offset center = rect.center;
+
+      final smilePaint = Paint()
+        ..style = PaintingStyle.stroke
+        ..strokeWidth = radius / 8;
+
+      canvas.drawCircle(center, radius, paint);
+
+      canvas.drawArc(
+          Rect.fromCircle(
+              center: center.translate(0, radius / 8), radius: radius / 2),
+          0,
+          Math.pi,
+          false,
+          smilePaint);
+
+      canvas.drawCircle(Offset(center.dx - radius / 2, center.dy - radius / 2),
+          radius / 8, Paint());
+
+      canvas.drawCircle(Offset(center.dx + radius / 2, center.dy - radius / 2),
+          radius / 8, Paint());
+    }
+  }
+
+  @override
+  bool shouldRepaint(SmilePainterLiveCamera oldDelegate) {
+    return imageSize != oldDelegate.imageSize || faces != oldDelegate.faces;
+  }
+}
+
+Rect _scaleRect({
+  @required Rect rect,
+  @required Size imageSize,
+  @required Size widgetSize,
+}) {
+  final double scaleX = widgetSize.width / imageSize.width;
+  final double scaleY = widgetSize.height / imageSize.height;
+
+  return Rect.fromLTRB(
+      rect.left.toDouble() * scaleX,
+      rect.top.toDouble() * scaleY,
+      rect.right.toDouble() * scaleX,
+      rect.bottom.toDouble() * scaleY);
 }
